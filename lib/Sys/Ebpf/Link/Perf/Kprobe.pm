581c21a616a737154230fbefa4852ba2d87f2d06
Takeru Hayasaka <hayatake396@gmail.com>
2024-09-14 05:12:33 +0900

    chore: remove comment

--- lib/Sys/Ebpf/Link/Perf/Kprobe.pm
+++ lib/Sys/Ebpf/Link/Perf/Kprobe.pm
@@ -4,9 +4,9 @@ use strict;
 use warnings;
 use utf8;
 
-use POSIX                       qw( close fcntl open sprintf );
-use Fcntl                       qw( F_GETFL );
-use IO::Handle                  qw( print );
+use POSIX;
+use Fcntl;
+use IO::Handle;
 use Sys::Ebpf::Link::Perf::Arch qw( platform_prefix );
 use open ':std', ':encoding(UTF-8)';
 
@@ -17,30 +17,30 @@ use constant {
     PERF_TYPE_TRACEPOINT   => 2,
     PERF_SAMPLE_RAW        => 1 << 10,
     PERF_FLAG_FD_CLOEXEC   => 0x00000008,
-    SYS_perf_event_open    => 298,    # x86_64 システムの場合。他のアーキテクチャでは異なる可能性があります。
+    SYS_perf_event_open    => 298,
     SYS_ioctl              => 16,
 };
 
 sub attach_kprobe {
     my ( $prog_fd, $kprobe_fn ) = @_;
 
-    # kprobe 関数名の抽出
+    # Extract kprobe function name
     my $func_name;
     if ( $kprobe_fn =~ m{^kprobe/(.+)$} ) {
         $func_name = $1;
     }
     else {
-        die "無効な kprobe 関数名です: $kprobe_fn";
+        die "Invalid kprobe function name: $kprobe_fn";
     }
 
     my $prefix = Sys::Ebpf::Link::Perf::Arch::platform_prefix();
     $func_name = $prefix . $func_name unless $func_name =~ /^$prefix/;
 
-    # ユニークなイベント名を生成
+    # Generate a unique event name
     my $current_pid = $$;
     my $event_name  = "p_${func_name}_${current_pid}";
 
-    # kprobe_events ファイルのパスを動的に取得
+    # Dynamically obtain the path to the kprobe_events file
     my $kprobe_events;
     if ( -e '/sys/kernel/debug/tracing/kprobe_events' ) {
         $kprobe_events = '/sys/kernel/debug/tracing/kprobe_events';
@@ -49,53 +49,53 @@ sub attach_kprobe {
         $kprobe_events = '/sys/kernel/tracing/kprobe_events';
     }
     else {
-        die "kprobe_events ファイルが見つかりません";
+        die "kprobe_events file not found";
     }
 
     print "kprobe_events: $kprobe_events\n";
     print "p:$event_name $func_name\n";
 
-    # kprobe イベントの作成
-    open my $fh, '>>', $kprobe_events or die "kprobe_events を開けません: $!";
+    # Create kprobe event
+    open my $fh, '>>', $kprobe_events or die "Cannot open kprobe_events: $!";
     $fh->autoflush(1);
     print $fh "p:$event_name $func_name\n"
-        or die "kprobe_events への書き込みに失敗しました: $!";
-    close $fh or warn "kprobe_events のクローズに失敗しました: $!";
+        or die "Failed to write to kprobe_events: $!";
+    close $fh or warn "Failed to close kprobe_events: $!";
 
-    # イベント ID の取得
+    # Obtain event ID
     my $id_file = $kprobe_events;
     $id_file =~ s/kprobe_events/events\/kprobes\/$event_name\/id/;
     unless ( -e $id_file ) {
-        die "イベント ID ファイルが存在しません。kprobe イベントの作成に失敗した可能性があります。";
+        die "Event ID file does not exist. Failed to create kprobe event.";
     }
-    open my $id_fh, '<', $id_file or die "イベント ID ファイルを開けません: $!";
+    open my $id_fh, '<', $id_file or die "Cannot open event ID file: $!";
     my $id = <$id_fh>;
     chomp $id;
     close $id_fh;
 
     my $pack_str = "L" . # type, __u32
-        "L" .            #size, __u32
-        "Q" .            #config, __u64
-        "Q" .            #sample_period, __u64
-        "Q" .            #sample_type, __u64
-        "Q" .            #read_format, __u64
-        "Q" .            #ビットフィールド (disabled, inherit, etc.), __u64
-        "L" .            #wakeup_events, __u32
-        "L" .            #bp_type, __u32
-        "Q" .            #config1 (bp_addr / kprobe_func / uprobe_path), __u64
-        "Q" .            #config2 (bp_len / kprobe_addr / probe_offset), __u64
-        "Q" .            #branch_sample_type, __u64
-        "Q" .            #sample_regs_user, __u64
-        "L" .            #sample_stack_user, __u32
-        "L" .            #clockid, __s32
-        "Q" .            #sample_regs_intr, __u64
-        "L" .            #aux_watermark. __u32
-        "S" .            #sample_max_stack, __u16
-        "S" .            #__reserved_2, __u16
-        "L" .            #aux_sample_size, __u32
-        "L" .            #__reserved_3, __u32
-        "Q" .            #sig_data, __u64
-        "Q";             #config3, __u64
+        "L" .            # size, __u32
+        "Q" .            # config, __u64
+        "Q" .            # sample_period, __u64
+        "Q" .            # sample_type, __u64
+        "Q" .            # read_format, __u64
+        "Q" .            # Bit field (disabled, inherit, etc.), __u64
+        "L" .            # wakeup_events, __u32
+        "L" .            # bp_type, __u32
+        "Q" .            # config1 (bp_addr / kprobe_func / uprobe_path), __u64
+        "Q" .            # config2 (bp_len / kprobe_addr / probe_offset), __u64
+        "Q" .            # branch_sample_type, __u64
+        "Q" .            # sample_regs_user, __u64
+        "L" .            # sample_stack_user, __u32
+        "L" .            # clockid, __s32
+        "Q" .            # sample_regs_intr, __u64
+        "L" .            # aux_watermark, __u32
+        "S" .            # sample_max_stack, __u16
+        "S" .            # __reserved_2, __u16
+        "L" .            # aux_sample_size, __u32
+        "L" .            # __reserved_3, __u32
+        "Q" .            # sig_data, __u64
+        "Q";             # config3, __u64
     my $attr_size = 0;
     $attr_size += { C => 1, S => 2, L => 4, Q => 8 }->{$_}
         for split //, $pack_str;
@@ -103,12 +103,12 @@ sub attach_kprobe {
     my $attr = pack(
         $pack_str,
         PERF_TYPE_TRACEPOINT,    # type, __u32
-        $attr_size,              # size (構造体全体のサイズ), __u32
+        $attr_size,              # size (total size of the structure), __u32
         $id,                     # config (kprobe_id), __u64
         1,                       # sample_period, __u64
         PERF_SAMPLE_RAW,         # sample_type, __u64
         0,                       # read_format, __u64
-        0,                       # ビットフィールド (disabled, inherit, etc.), __u64
+        0,                       # Bit field (disabled, inherit, etc.), __u64
         1,                       # wakeup_events, __u32
         0,                       # bp_type, __u32
         0,    # config1 (bp_addr / kprobe_func / uprobe_path), __u64
@@ -118,7 +118,7 @@ sub attach_kprobe {
         0,    # sample_stack_user, __u32
         0,    # clockid, __s32
         0,    # sample_regs_intr, __u64
-        0,    # aux_watermark. __u32
+        0,    # aux_watermark, __u32
         0,    # sample_max_stack, __u16
         0,    # __reserved_2, __u16
         0,    # aux_sample_size, __u32
@@ -127,9 +127,9 @@ sub attach_kprobe {
         0,    # config3, __u64
     );
 
-    # perf_event_open システムコールの呼び出し
-    my $target_pid = -1;                     # すべてのプロセスを監視
-    my $cpu        = 0;                      # すべてのCPUで監視
+    # Call perf_event_open system call
+    my $target_pid = -1;                     # Monitor all processes
+    my $cpu        = 0;                      # Monitor on all CPUs
     my $group_fd   = -1;
     my $flags      = PERF_FLAG_FD_CLOEXEC;
 
@@ -139,13 +139,13 @@ sub attach_kprobe {
     if ( $perf_fd < 0 ) {
         my $errno     = $! + 0;
         my $error_msg = $!;
-        die sprintf( "perf イベントのオープンに失敗しました: %s (errno: %d)\n",
+        die sprintf( "Failed to open perf event: %s (errno: %d)\n",
             $error_msg, $errno );
     }
 
     print "perf_event_open successful. File descriptor: $perf_fd\n";
 
-    # BPF プログラムを perf イベントにアタッチ
+    # Attach BPF program to perf event
     $! = 0;    # reset errno
     my $res
         = syscall( &SYS_ioctl, $perf_fd, PERF_EVENT_IOC_SET_BPF, $prog_fd );
@@ -153,26 +153,26 @@ sub attach_kprobe {
     print "ioctl res: $res\n";
     if ( !defined $res || $res < 0 ) {
         my $error_msg = $!;
-        die sprintf( "BPF プログラムのアタッチに失敗しました: %s (errno: %d)\n",
+        die sprintf( "Failed to attach BPF program: %s (errno: %d)\n",
             $error_msg, $errno );
     }
 
     print "BPF program attached successfully\n";
 
-    # perf イベントを有効化
+    # Enable perf event
     $!     = 0;    # reset errno
                    # $res = ioctl($perf_fh, PERF_EVENT_IOC_ENABLE, 0)|| -1;
     $res   = syscall( &SYS_ioctl, $perf_fd, PERF_EVENT_IOC_ENABLE, 0 );
     $errno = $! + 0;
     if ( !defined $res || $res < 0 ) {
         my $error_msg = $!;
-        die sprintf( "perf イベントの有効化に失敗しました: %s (errno: %d)\n",
+        die sprintf( "Failed to enable perf event: %s (errno: %d)\n",
             $error_msg, $errno );
     }
 
     print "perf event enabled successfully\n";
 
-    # デタッチ時に必要な情報を返す
+    # Return information necessary for detachment
     return {
         perf_fd       => $perf_fd,
         event_name    => $event_name,
@@ -198,19 +198,19 @@ sub detach_kprobe {
     my $event_name    = $kprobe_info->{event_name};
     my $kprobe_events = $kprobe_info->{kprobe_events};
 
-    # perf イベントを無効化
+    # Disable perf event
     my $res = syscall( SYS_ioctl, $perf_fd, PERF_EVENT_IOC_DISABLE, 0 );
     if ( $res != 0 ) {
-        warn "perf イベントの無効化に失敗しました: $!";
+        warn "Failed to disable perf event: $!";
     }
 
-    # perf イベントのファイルディスクリプタをクローズ
+    # Close the file descriptor of the perf event
     close($perf_fd);
 
-    # kprobe イベントの削除
-    open my $fh, '>>', $kprobe_events or warn "kprobe_events を開けません: $!";
+    # Remove kprobe event
+    open my $fh, '>>', $kprobe_events or warn "Cannot open kprobe_events: $!";
     $fh->autoflush(1);
-    print $fh "-:$event_name\n" or warn "kprobe_events への書き込みに失敗しました: $!";
-    close $fh                   or warn "kprobe_events のクローズに失敗しました: $!";
+    print $fh "-:$event_name\n" or warn "Failed to write to kprobe_events: $!";
+    close $fh                   or warn "Failed to close kprobe_events: $!";
 }
 1;
